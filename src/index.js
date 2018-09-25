67bb07b3f016c1849d2f03623efbdbc98def0541
Garin87 <kamrader@gmail.com>
2018-09-25 17:33:33 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+// your solution
+
+	var stack = [];
+	var arr = str.split('');
+	var temp;
+
+	for(var i  = 0; i < arr.length; i++){
+
+		if ( arr[i] === '(' || arr[i] === '{' 
+		   || arr[i] === '['  || arr[i] === '|'){
+			stack.push(arr[i]);
+			continue;
+		}
+		if ( arr[i] == ')' || arr[i] == '}'
+		    || arr[i] == ']'  || arr[i] == '|'){
+			temp = stack.pop();
+
+			if( (temp == '(' && arr[i] == ')') ||
+				(temp == '[' && arr[i] == ']') ||
+				(temp == '{' && arr[i] == '}') || 
+				(temp == '|' && arr[i] == '|') 
+				) continue;  	
+			else return false;  
+        }	
+    }
+
+    if( stack.length > 0 ) return false;
+
+    return true;
 }
